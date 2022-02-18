3a9ed231d6bf2d8995dd91028f24ca510f18d6f9
Cian <99752928+NilWo@users.noreply.github.com>
2022-02-18 21:20:17 +0100

    infinite runs via y/n & forever loop

--- src/choiceclass.java
+++ src/choiceclass.java
@@ -3,47 +3,52 @@ import java.util.Scanner;
 public class choiceclass {
 
     String rechentyp;
+    String rerun;
     public static taschenrechner rechner2;
 
     public void choice() {
-        rechner2=new taschenrechner();
+        for (; ; ) {
+            rechner2 = new taschenrechner();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Wie möchtest du rechnen? Summe, Subtraktion, Multiplikation, Division, Potenz oder Wurzel?");
-        rechentyp = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Wie möchtest du rechnen? Summe, Subtraktion, Multiplikation, Division, Potenz oder Wurzel?");
+            rechentyp = scanner.nextLine();
 
-        if (rechentyp.equals("Summe")) {
-            rechner2.erfassung();
-            rechner2.summe();
-        }
+            if (rechentyp.equals("Summe")) {
+                rechner2.erfassung();
+                rechner2.summe();
+            } else if (rechentyp.equals("Subtraktion")) {
+                rechner2.erfassung();
+                rechner2.subtraktion();
+            } else if (rechentyp.equals("Multiplikation")) {
+                rechner2.erfassung();
+                rechner2.multiplikation();
+            } else if (rechentyp.equals("Division")) {
+                rechner2.erfassung();
+                rechner2.division();
+            } else if (rechentyp.equals("Potenz")) {
+                rechner2.erfassung();
+                rechner2.potenz();
+            } else if (rechentyp.equals("Wurzel")) {
+                rechner2.erfassung();
+                rechner2.wurzel();
+            } else {
+                System.out.println("Falsche Eingabe!");
+            }
+            System.out.println();
+            System.out.println("Möchtest du noch eine Rechnung tätigen? y/n");
+            rerun = scanner.nextLine();
 
-        else if (rechentyp.equals("Subtraktion")) {
-            rechner2.erfassung();
-            rechner2.subtraktion();
-        }
-
-        else if (rechentyp.equals("Multiplikation")) {
-            rechner2.erfassung();
-            rechner2.multiplikation();
-        }
-
-        else if (rechentyp.equals("Division")) {
-            rechner2.erfassung();
-            rechner2.division();
-        }
-
-        else if (rechentyp.equals("Potenz")) {
-            rechner2.erfassung();
-            rechner2.potenz();
-        }
-
-        else if (rechentyp.equals("Wurzel")) {
-            rechner2.erfassung();
-            rechner2.wurzel();
-        }
-
-        else{
-        System.out.println("Falsche Eingabe!");
+            if (rerun.equals("y")) {
+                System.out.println("Wunderbar!");
+                System.out.println();
+            } else if (rerun.equals("n")) {
+                System.out.println("Schade marmelade!");
+                break;
+            } else {
+                System.out.println("Falsche Eingabe!");
+                break;
+            }
         }
     }
 }
